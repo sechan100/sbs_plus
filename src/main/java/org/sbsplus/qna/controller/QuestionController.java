7572f444cd6182bba1af993a969941026eb241ea
parkhyeo <hyeonjae1023111@gmail.com>
2023-09-12 10:19:30 +0900

    questionForm 구현으로 빈칸시 오류 메세지 표시

--- src/main/java/org/sbsplus/qna/controller/QuestionController.java
+++ src/main/java/org/sbsplus/qna/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package org.sbsplus.qna.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.sbsplus.qna.QuestionForm;
 import org.sbsplus.qna.service.QuestionService;
 import org.sbsplus.qna.entity.Question;
 import org.sbsplus.qna.repository.QuestionRepository;
@@ -8,6 +10,7 @@ import org.sbsplus.type.Category;
 import org.sbsplus.util.Rq;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,13 +36,16 @@ public class QuestionController {
         return "/qna/question_detail";
     }
     @GetMapping("/create")
-    public String questionCreate(Model model) {
+    public String questionCreate(Model model, QuestionForm questionForm) {
         model.addAttribute("categories", Category.getCategories());
         return "/qna/question_form";
     }
     @PostMapping("/create")
-    public String questionCreate(@RequestParam String subject, @RequestParam String content, @RequestParam String category) {
-        this.questionService.create(subject, content, category);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, @RequestParam String category) {
+        if(bindingResult.hasErrors()) {
+            return "/qna/question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getSubject(), category);
         return "redirect:/question";
     }
 }
