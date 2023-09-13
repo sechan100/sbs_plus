3900cd8554f66aee2828ead1325ecacca063fad1
Sechan Beak <sechan100@gmail.com>
2023-09-13 12:18:03 +0900

    댓글 삭제 기능 추가 (#9)
    * article 추천 및 취소 로직 완성
    
    * 댓글 작성 기능 완성
    
    * 댓글 수정 기능 form 데이터 안보내지는 버그 발견
    
    * 댓글 수정 기능 완료
    
    * 댓글 삭제 기능

--- src/main/java/org/sbsplus/admin/AdminServiceImpl.java
+++ src/main/java/org/sbsplus/admin/AdminServiceImpl.java
@@ -22,19 +22,7 @@ public class AdminServiceImpl implements AdminService {
     
     @Override
     public boolean isAdmin() {
-        
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        
-        Collection<? extends GrantedAuthority> grantedAuthorities = authentication.getAuthorities();
-        for(GrantedAuthority grantedAuthority: grantedAuthorities){
-            
-            // admin 권한 확인
-            if(grantedAuthority.getAuthority().equals("ROLE_ADMIN")){
-                return true;
-            }
-        }
-        
-        return false;
+        return rq.isAdmin();
     }
     
     @Override
