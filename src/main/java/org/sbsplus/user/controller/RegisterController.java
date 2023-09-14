09b838ec9ee3c56901d76cb9f52f70af80abe7b5
sechan100 <sechan100@gmail.com>
2023-09-14 12:54:03 +0900

    devtools 설정

--- src/main/java/org/sbsplus/user/controller/RegisterController.java
+++ src/main/java/org/sbsplus/user/controller/RegisterController.java
@@ -12,7 +12,6 @@ import org.sbsplus.user.service.UserService;
 import org.sbsplus.util.EmailAuthentication;
 import org.sbsplus.util.Rq;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
