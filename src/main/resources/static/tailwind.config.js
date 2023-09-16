9947a447e79258238776ecdbc264bd6cb6d1a79d
sechan100 <sechan100@gmail.com>
2023-09-17 01:37:38 +0900

    커뮤니티 페이지 pagination 추가

--- src/main/resources/static/tailwind.config.js
+++ src/main/resources/static/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: {
-        enabled: true,
-        content: ["../templates/thymeleaf/**/*.html"],
-      },
+
+// 실제 사용중인 클래스들만 컴파일
+//    purge: {
+//        enabled: true,
+//        content: ["../templates/thymeleaf/**/*.html"],
+//      },
+
+// 모든 클래스 컴파일
+  content: ["../templates/thymeleaf/**/*.html"],
+
+
+
   theme: {
     extend: {},
   },
