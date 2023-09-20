8ef4e3706921c726893a4156f7481892d749bd59
sechan100 <sechan100@gmail.com>
2023-09-20 09:58:37 +0900

    게시글 페이지 (게시글부/댓글부) 게시글 부 완성

--- src/main/java/org/sbsplus/domain/cummunity/controller/CommentController.java
+++ src/main/java/org/sbsplus/domain/cummunity/controller/CommentController.java
@@ -4,10 +4,12 @@ package org.sbsplus.domain.cummunity.controller;
 import lombok.RequiredArgsConstructor;
 import org.sbsplus.domain.cummunity.dto.CommentDto;
 import org.sbsplus.domain.cummunity.service.ArticleService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,23 +17,27 @@ public class CommentController {
     
     private final ArticleService articleService;
     
-    
+    // 댓글 작성 & 수정
     @PostMapping("/comment/write")
+    @ResponseBody
     public String commentWrite
             ( @RequestParam Integer articleId
             , @RequestParam(required = false) Integer commentId
             , CommentDto commentDto){
         
         
-        // 아이디 여부에 따라서 서비스 로직에서 분기
+        // 아이디 있음 -> 수정
+        // 아이디 없음 -> 등록
         commentDto.setId(commentId);
+        
         articleService.saveComment(articleId, commentDto);
         
         
         
-        return "redirect:/article/" + articleId;
+        return "댓글 작성 성공이야~~~";
     }
-
+    
+    // 댓글 삭제
     @GetMapping("/comment/delete")
     public String deleteComment(@RequestParam Integer articleId, @RequestParam Integer commentId){
         
@@ -40,6 +46,7 @@ public class CommentController {
         return "redirect:/article/" + articleId;
     }
   
+    
     // 댓글 좋아요
     @GetMapping("/comment/like")
     public String commentLike(@RequestParam Integer articleId, @RequestParam Integer commentId){
