3c5f872d4d6baa3800c85b39b284d1846a797f35
sechan100 <sechan100@gmail.com>
2023-09-21 11:27:23 +0900

    내 게시글 페이지 서버

--- src/main/java/org/sbsplus/domain/cummunity/entity/Article.java
+++ src/main/java/org/sbsplus/domain/cummunity/entity/Article.java
@@ -9,8 +9,12 @@ import org.sbsplus.domain.user.entity.User;
 import org.sbsplus.general.type.Category;
 import org.sbsplus.util.Datetime;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static jakarta.persistence.CascadeType.*;
 import static jakarta.persistence.EnumType.*;
@@ -52,5 +56,54 @@ public class Article extends Datetime {
     
     public void increaseHit(){
         this.setHit(this.getHit()+1);
+    }
+    
+    public String extractThumbNail(){
+        Pattern pattern = Pattern.compile("<img[^>]*>");
+        Matcher matcher = pattern.matcher(content);
+        
+        if (matcher.find()) {
+            
+            return matcher.group();
+            
+        } else {
+            return "<img class='w-1/16 h-1/16' src='https://ifh.cc/g/9AqA7q.png' />";
+        }
+    }
+    
+    public String convertRelativeTime(){
+        int SEC = 60;
+        int MIN = 60;
+        int HOUR = 24;
+        int DAY = 30;
+        int MONTH = 12;
+        
+        LocalDateTime now = LocalDateTime.now();
+        
+        long diffTime = super.getCreateAt().until(now, ChronoUnit.SECONDS); // now보다 이후면 +, 전이면 -
+        
+        String msg = null;
+        if (diffTime < SEC){
+            return diffTime + "초전";
+        }
+        diffTime = diffTime / SEC;
+        if (diffTime < MIN) {
+            return diffTime + "분 전";
+        }
+        diffTime = diffTime / MIN;
+        if (diffTime < HOUR) {
+            return diffTime + "시간 전";
+        }
+        diffTime = diffTime / HOUR;
+        if (diffTime < DAY) {
+            return diffTime + "일 전";
+        }
+        diffTime = diffTime / DAY;
+        if (diffTime < MONTH) {
+            return diffTime + "개월 전";
+        }
+        
+        diffTime = diffTime / MONTH;
+        return diffTime + "년 전";
     }
 }
--- src/main/java/org/sbsplus/domain/user/entity/User.java
+++ src/main/java/org/sbsplus/domain/user/entity/User.java
@@ -50,7 +50,6 @@ public class User {
     
     
     
-    
     @Override
     public boolean equals(Object o) {
         if(this == o)
